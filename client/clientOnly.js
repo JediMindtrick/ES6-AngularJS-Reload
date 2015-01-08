4beba79193ef726b483d58499fb4a582f9c35098
JediMindtrick <brandonjwilhite@gmail.com>
2015-01-08 16:08:12 -0500

    6to5 is part of buid/dev process

---
+++ client/clientOnly.js
@@ -0,0 +1,3 @@
+//this should only be found on the client
+//------ lib.js ------
+export const fizz = 'bazz';
---
+++ server/serverOnly.js
@@ -0,0 +1,3 @@
+//this should only be found on the server
+//------ lib.js ------
+export const fizz = 'buzz';
