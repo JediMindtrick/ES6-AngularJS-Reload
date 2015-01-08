3bfe19a7e1f965716fdb6170cf3359dc823875f2
JediMindtrick <brandonjwilhite@gmail.com>
2015-01-08 07:46:59 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var fs = require('fs');
+var path = require('path');
+var http = require('http');
+reload = require('reload');
+var bodyParser = require('body-parser');
+
+var app = express();
+//middleware
+app.set('views', path.join(__dirname, 'spa'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded())
+app.use(express.static(path.join(__dirname, 'spa')));
+app.engine('html', require('ejs').renderFile);
+
+//routes
+app.get('/', function(req, res) {
+  res.render('index.html');
+});
+
+//start
+var server = app.listen(8080);
+
+//add live reload functionality
+reload.all(server,app);
+console.log('listening on ' + 8080);
